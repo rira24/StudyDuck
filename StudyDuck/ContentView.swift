31a7f6cede1e2ebaec05f39a18ebcdc8bda76547
Rithika <rithu427@gmail.com>
2023-07-18 13:38:34 -0500

    Initial Commit

---
+++ StudyDuck/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StudyDuck
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Study Duck Project")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StudyDuck/StudyDuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyDuckApp.swift
+//  StudyDuck
+//
+//  Created by Scholar on 7/18/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyDuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
