a1c1d71aa56c33d687a40e8a9fd794dfeef96efd
Rithika <rithu427@gmail.com>
2023-07-18 14:10:26 -0500

    Basic Buttons Added

--- StudyDuck/ContentView.swift
+++ StudyDuck/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  StudyDuck
+//  StudyDuck_homepage
 //
 //  Created by Scholar on 7/18/23.
 //
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Study Duck Project!!")
-            Text("STart")
+        ZStack {
+            Color(.systemGreen)
+                .ignoresSafeArea()
+            
+            VStack{
+                Text("StudyDuck")
+                    .font(.title)
+                    .fontWeight(.bold)
+              
+                
+                Button("Calendar") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }.font(.title2)
+                    .buttonStyle(.borderedProminent)
+                   // .tint(.yellow)
+                HStack{
+                    Button("Today's Schedule") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.title2)
+                        .buttonStyle(.borderedProminent)
+                    // .tint(.yellow)
+                    Button("Duck") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.title2)
+                        .buttonStyle(.borderedProminent)
+                        
+                }
+                
+                HStack {
+                    Button("Resources") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.title2)
+                        .buttonStyle(.borderedProminent)
+                      
+                    
+                    Button("Calculator") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.title2)
+                        .buttonStyle(.borderedProminent)
+                       // .tint(.yellow)
+                    Button("Notes") {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.font(.title2)
+                        .buttonStyle(.borderedProminent)
+                       // .tint(.yellow)
+                }
+
+            }
+            
+            
+            
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
