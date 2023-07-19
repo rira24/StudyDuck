2bfdc7280f6a0c8a4b743490b17663060e8ac990
Lavina Cheung <aoikkes@gmail.com>
2023-07-19 13:03:38 -0500

    Added a random message button for the duck

--- StudyDuck/Duck.swift
+++ StudyDuck/Duck.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct Duck: View {
+@State private var clickMe = "Click Me"
+@State private var messages = ["You're working hard!", "Keep up the good work!", "You'll do great on the test!"]
+func pickMsg() -> String {
+    let random = Int.random(in: 0...2)
+    let message = messages[random]
+        return message
+    }
     var body: some View {
-        Text("This is the duck")
+        VStack {
+            Button(clickMe) {
+                let randomMesssge = pickMsg()
+                clickMe = "\(randomMesssge)"
+            }
+            .font(.title)
+        }
     }
 }
 
