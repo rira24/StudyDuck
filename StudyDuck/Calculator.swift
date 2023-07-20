f2168320f318d417c8c1693e6328ba7ce03d9ce5
emily <eli.mhs24@gmail.com>
2023-07-20 11:15:21 -0500

    calculator commit 2

--- StudyDuck/Calculator.swift
+++ StudyDuck/Calculator.swift
@@ -13,59 +13,62 @@ struct Calculator: View {
     @State private var dateLearned = Date()
     @State private var dateTest = Date()
     @State var isDifficult = false
+    @State var timeInBetween = 0
+    @State var content = ""
+    @State var buttonName = "Input data"
     var body: some View {
         
         NavigationStack {
-            VStack(spacing: 40.0) {
-               
-                Text("Calculate my schedule")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .multilineTextAlignment(.center)
-                HStack(spacing: 40.0){
+            ZStack{
+                Color(.systemGreen)
+                    .ignoresSafeArea()
+                VStack(spacing: 40.0) {
+                   
+                    Text("Calculate my schedule")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
                     
-                
-                    VStack{
-                        Text("Date learned")
-                            .multilineTextAlignment(.leading)
-                        DatePicker(selection: $dateLearned, displayedComponents: .date, label: {  })
-                            .padding()
+                    
+                    HStack{
+                        VStack{
+                            Text("Subject")
+                                .multilineTextAlignment(.leading)
+                            TextField("Type here...", text: $subject).multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            
+                        }
                     }
-                    VStack{
-                        Text("Test Date")
-                            .multilineTextAlignment(.center)
-                        DatePicker(selection: $dateTest, displayedComponents: .date, label: {  }).padding()
+                    HStack(spacing: 40.0){
                         
+                    
+                        VStack{
+                            Text("Date learned")
+                                .multilineTextAlignment(.leading)
+                            DatePicker(selection: $dateLearned, displayedComponents: .date, label: {  })
+                                .padding()
+                        }
+                        VStack{
+                            Text("Test Date")
+                                .multilineTextAlignment(.center)
+                            DatePicker(selection: $dateTest, displayedComponents: .date, label: {  }).padding()
+                            
+                        }
                     }
-                }
-                HStack{
+                   
+                    
                     VStack{
-                        Text("Subject")
+                        Text("Content")
                             .multilineTextAlignment(.leading)
-                        TextField("Type here...", text: $subject).multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        TextField("Type here...", text: $content).multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     }
-                    Toggle(isOn: $isDifficult) {
-                        Text("Is it difficult?")
-                            .multilineTextAlignment(.center)
+                    
+                    NavigationLink(destination: calculatedResultView(timeBetween: Int(dateTest.timeIntervalSinceReferenceDate - dateLearned.timeIntervalSinceReferenceDate), subject: self.subject, startDate: self.dateLearned)) {
+                        Text("Input data")
                     }
-                }
-                
-                VStack{
-                    Text("Content")
-                        .multilineTextAlignment(.leading)
-                    TextField("Type here...", text: $subject).multilineTextAlignment(/*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
-                }
-                
-                Button("Calculate"){
-                    clicked=true
-                }
-            } .padding()
-        }
-
-        if clicked{
-            calculatedResultView(startDate: self.dateLearned, endDate: self.dateTest, subject: self.subject)
-        }
-       
+                }.padding()//end of vstack
+                    
+            }//end of zstack
+        }//end of navigation stack
     }
 }
 
--- StudyDuck/calculatedResultView.swift
+++ StudyDuck/calculatedResultView.swift
@@ -6,21 +6,75 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct calculatedResultView: View {
-    @State var startDate = Date()
-    @State var endDate = Date()
+    @State var timeBetween = 0
     @State var subject : String
-    
+    @State var array = [Int]()
+    @State var prevNum = 0
+    @State var nextNum = 0
+    @State var startDate = Date()
+    @State var timeInterval = 0.0
+    @State var startDateInSecs = 0
+    @State var currentDateHolder = 0
+    let dateFormatter = DateFormatter()
+    @State var studyDaysArray = [String]()
+    @State var clicked = false
+    @State var buttonName = "Calculate"
+   
     var body: some View {
-        Text("You should review \(subject) on... ")
-        
+        ZStack{
+            Color(.systemGreen)
+                .ignoresSafeArea()
+            VStack(spacing: 50.0){
+                Button(buttonName){
+                    buttonName = ""
+                    clicked = true
+                    prevNum=timeBetween/(3600*24)
+                    array.append(prevNum)
+                    repeat {
+                        
+                        nextNum=prevNum/2
+                        array.append(nextNum)
+                        prevNum = nextNum
+                    }while(prevNum>1)
+                    
+                    dateFormatter.dateFormat = "YY, MMM d"
+                    timeInterval = startDate.timeIntervalSince1970
+                    startDateInSecs = Int(timeInterval)
+                    currentDateHolder = 0
+                    for day in array{
+                        currentDateHolder = startDateInSecs + (day*3600*24)
+                        studyDaysArray.append(dateFormatter.string(from: Date(timeIntervalSince1970: TimeInterval(currentDateHolder))))
+                        
+                    }
+                }
+                Text("You should review \(subject) on... ")
+                if clicked{
+                    if((timeBetween/(3600*24))==0){
+                        Text("WHAT? Study today!!")
+                        Text("Your test is soon, better start reviewing!")
+                    }else if((timeBetween/(3600*24))==1){
+                        ForEach(studyDaysArray, id: \.self){studyDay in
+                            Text(studyDay)
+                        }
+                        Text("You have \(timeBetween/(60*60*24)) day before your test!")
+                    }else{
+                        ForEach(studyDaysArray, id: \.self){studyDay in
+                            Text(studyDay)
+                        }
+                        Text("You have \(timeBetween/(60*60*24)) days before your test!")
+                    }
+                    
+                }
+            }
+        }
     }
-    
 }
 
 struct calculatedResultView_Previews: PreviewProvider {
     static var previews: some View {
-        calculatedResultView(startDate: Date(), endDate: Date(), subject: "")
+        calculatedResultView(timeBetween: 0,subject: "",startDate: Date())
     }
 }
