7d71d491d137cfee433368b957e6f9947232570d
emily <eli.mhs24@gmail.com>
2023-07-20 11:26:38 -0500

    more motivational msgs commit

--- StudyDuck/Duck.swift
+++ StudyDuck/Duck.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 struct Duck: View {
 @State private var clickMe = "Message from Dave: Click Me"
-@State private var messages = ["You're working hard!", "Keep up the good work!", "You'll do great on the test!"]
+@State private var messages = ["You're working hard!", "Keep up the good work!", "You'll do great on the test!","You got this, remember to take breaks!","You're doing great :)","I believe in you!!","I think you're amazing","Starting is always hard, don't worry too much!","Don't give up, remember success is not a straight line","I know you'll get through this","Quack quack, that means you're awesome in Duck","You will do great things in life"]
 func pickMsg() -> String {
-    let random = Int.random(in: 0...2)
+    let random = Int.random(in: 0...messages.count)
     let message = messages[random]
         return message
     }
