3048f580bea0db8bc35b6fd3cc5032820f890b13
emily <eli.mhs24@gmail.com>
2023-07-21 09:34:57 -0500

    pomodoro

--- StudyDuck/Pomodoro.swift
+++ StudyDuck/Pomodoro.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct Pomodoro: View {
     var body: some View {
-        Text("Pomodoro Technique")
+        ZStack{
+            Color("lighterGreen")
+                .ignoresSafeArea()
+            VStack{
+                Text("Pomodoro Technique")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Spacer()
+                    .frame(height: 20)
+                
+                Text("\nThe Pomodoro Technique is a time management method. The method is used while studying for a while then taking a short break; a longer break is then added when studying for an hour. An example would be studying for 25 minutes then taking a 5 minute break. After doing this 4 times, take a 15 minute break then move on to the next subject. You can make your own routine by basing it off on how long and how much you want to study a certain subject;  you don’t have to stick to the 25/5 method.  The Pomodoro Technique is effective because it gives motivation and helps fight against distractions. \n")
+                    
+                    .multilineTextAlignment(.center)
+                    .background(Rectangle().foregroundColor(.white)).shadow(radius: 15)
+                    .cornerRadius(15)
+                    .padding()
+                    
+                    
+                
+            }
+            .padding()
+            
+        }
     }
 }
 
